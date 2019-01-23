3fab1eea0a5bbd6f217a2330ca076f8a80f72b50
suwei <910933955@qq.com>
2019-01-23 16:00:13 +0800

    Updates

---
+++ precache-manifest.b773393b479db5e391585efaf858fa3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd3b51bb8fd803a2c583",
+    "url": "/noob/static/css/main.02261e54.chunk.css"
+  },
+  {
+    "revision": "bd3b51bb8fd803a2c583",
+    "url": "/noob/static/js/main.bd3b51bb.chunk.js"
+  },
+  {
+    "revision": "edd724ac03814cbcabd7",
+    "url": "/noob/static/css/1.44c37766.chunk.css"
+  },
+  {
+    "revision": "edd724ac03814cbcabd7",
+    "url": "/noob/static/js/1.edd724ac.chunk.js"
+  },
+  {
+    "revision": "baba066e10b6d615fe53",
+    "url": "/noob/static/js/runtime~main.baba066e.js"
+  },
+  {
+    "revision": "b6d9694cc3eb32b35e8bcfbef20e1524",
+    "url": "/noob/static/media/logo.b6d9694c.png"
+  },
+  {
+    "revision": "97e2db02a1855870f13a6988b8eeedeb",
+    "url": "/noob/static/media/isetan.97e2db02.jpg"
+  },
+  {
+    "revision": "eafa77b14eb5ae9f585fe3f9e183e49c",
+    "url": "/noob/static/media/pro1.eafa77b1.jpg"
+  },
+  {
+    "revision": "36fc899a22572323cae1e9af7a57e818",
+    "url": "/noob/static/media/pro2.36fc899a.jpg"
+  },
+  {
+    "revision": "067495e4f22932d6fe680aa062dd1474",
+    "url": "/noob/static/media/Avator.067495e4.png"
+  },
+  {
+    "revision": "23a477650d1c345b770cfc8de640dbac",
+    "url": "/noob/index.html"
+  }
+];
